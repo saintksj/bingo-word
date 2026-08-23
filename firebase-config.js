5345bab44443514b40b7615e6b5a3c81d11ca945
saintksj <saintksj74@gmail.com>
2026-08-23 14:16:19 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDDHh4dsBbA0rV7wcQRhVNx7p-zFlD-oug",
+  authDomain: "word-bingo-3c880.firebaseapp.com",
+  databaseURL: "https://word-bingo-3c880-default-rtdb.firebaseio.com",
+  projectId: "word-bingo-3c880",
+  storageBucket: "word-bingo-3c880.firebasestorage.app",
+  messagingSenderId: "104917889567",
+  appId: "1:104917889567:web:d1fc9353fa8d3a96401b2a"
+};
